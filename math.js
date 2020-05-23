87badfab956a913dca055f55a6a39a7979f28341
Md. Abu Bakar Siddique <abms.siddique@gmail.com>
2020-05-24 02:19:26 +0600

    Added Math.js

---
+++ math.js
@@ -0,0 +1,47 @@
+
+/* Math.round() returns the value of x rounded to its nearest integer */
+var round = Math.round(4.7);    
+console.log(round);
+
+var round = Math.round(4.4);
+console.log(round);
+
+/* Math.pow() returns the value of x to the power of y */
+
+var power = Math.pow(8,2);
+console.log(power);
+
+/* Math.sqrt() returns the square root of x*/
+var square = Math.sqrt(64)
+console.log(square);
+
+/* Math.abs() returns the absolute (positive) value of x*/
+
+var abs = Math.abs(-4.5)
+console.log(abs);
+
+/* Math.ceil() returns the value of x rounded up to its nearest integer*/
+
+var ceil = Math.ceil(4.4);
+console.log(ceil);
+
+/* Math.floor() returns the value of x rounded down to its nearest integer:*/
+var floor = Math.floor(4.7)
+console.log(floor);
+
+/* Math.random() */
+
+var random = Math.random();  
+console.log(random);
+
+
+for(i=0;i<10; i++){
+    var random = Math.floor(Math.random() * 101); 
+    console.log(random);
+}
+
+
+
+
+
+
