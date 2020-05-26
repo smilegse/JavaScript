99ec1f90656a7cd39a79a8a1536f1c64a3d56aa0
Md. Abu Bakar Siddique <=abms.siddique@gmail.com>
2020-05-26 22:31:54 +0600

    modified whileloop. file

--- whileloop.js
+++ whileloop.js
@@ -1,8 +1,30 @@
 
 /* While loop */
 
-var i = 1;
-while (i < 10) {
+// var i = 1;
+// while (i < 10) {
+//     console.log("The number is " + i);
+//     i++;
+// }
+
+/* The Do/While Loop */
+// var text = "";
+// var i = 0;
+
+// do {
+//     console.log("The number is " + i);
+//     i++;
+// }
+// while (i < 11)
+
+var cars = ["BMW", "Volvo", "Saab", "Ford"];
+var i = 0;
+// while (cars[i]) {
+//     console.log("Car Brand Name: " + cars[i]);
+//     i++;
+// }
+
+for (; cars[i];) {
+    console.log("Car Brand Name: " + cars[i]);
     i++;
-    console.log("The number is " + i);
 }
